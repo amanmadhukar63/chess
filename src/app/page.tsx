ced9724b11c5c4d9989a2aa7a7d5931fc828b732
Aman Madhukar <amanmadhukar@MacBook-Air.local>
2025-03-20 17:48:43 +0530

    added button to redirect two player game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,6 +34,12 @@ export default function Home() {
             >
             Join
           </div>
+          <div
+            className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            onClick={() => router.push("/board")}
+            >
+              2 Player Game
+          </div>
         </div>
       </main>
       </div>
